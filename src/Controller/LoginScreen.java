407799daa49a214ed32ca342cc54882f3eb0c8e1
Kgre123 <kgre123@wgu.edu>
2022-04-06 10:00:20 -0700

    4/6 1pm

--- src/Controller/LoginScreen.java
+++ src/Controller/LoginScreen.java
@@ -1,4 +1,52 @@
 package Controller;
 
-public class LoginScreen {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginScreen implements Initializable {
+
+    @FXML
+    public Label UserLoginLabel;
+
+    @FXML
+    public Button LoginButton;
+
+    @FXML
+    public Button CancelButton;
+
+    @FXML
+    public Label UsernameLabel;
+
+    @FXML
+    public Label PasswordLabel;
+
+    @FXML
+    public TextField UsernameText;
+
+    @FXML
+    public TextField PasswordText;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void onActionLogin(ActionEvent actionEvent) {
+    }
+
+    /**
+     * This method exits the program.
+     * @param actionEvent when the button is clicked
+     */
+    public void onActionCancel(ActionEvent actionEvent) {
+
+        System.exit(0);
+    }
 }
